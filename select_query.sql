122c8f9a89fe222b4ed40abdf5b45ef5b3ea109d
DrunkenPhilosoph <130548981+DrunkenPhilosoph@users.noreply.github.com>
2024-01-07 22:04:07 +0300

    Update select_query.sql
    fix scripts

--- select_query.sql
+++ select_query.sql
@@ -2,13 +2,16 @@
 
 SELECT track_name, duration
 FROM tracks
-ORDER BY duration DESC
-LIMIT 1;
+WHERE duration = (SELECT max(duration) FROM tracks);
 
 SELECT track_name, duration
 FROM tracks
 WHERE duration > 210
 ORDER BY duration desc;
+
+SELECT compilation_name, release_year
+from compilations
+WHERE release_year >= 2018 AND release_year <= 2020;
 
 SELECT artist_name
 FROM artists
@@ -16,7 +19,7 @@ WHERE artist_name NOT LIKE '% %';
 
 SELECT track_name
 FROM tracks
-WHERE track_name LIKE '%my%';
+WHERE track_name iLIKE '%my%';
 
 --Задание 3
 
@@ -40,7 +43,7 @@ SELECT artist_name
 FROM artists a 
 LEFT JOIN artists_albums aa ON a.artist_id = aa.artist_id 
 LEFT JOIN albums a2 ON a2.album_id = aa.album_id 
-WHERE a2.release_year != 2020;
+WHERE a.artist_name not IN (select artist_name from albums a3 left join artists_albums aa2 on a3.album_id = aa2.album_id left join artists a4 on a4.artist_id = aa2.artist_id where release_year = 2020);
 
 SELECT compilation_name
 FROM compilations c 
